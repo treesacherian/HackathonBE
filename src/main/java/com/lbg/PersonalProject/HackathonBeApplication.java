a22092f3d20fb5b40a29aff5f11f6da6bbb46592
treesacherian <treesa.cherian@gmail.com>
2024-05-07 11:05:06 +0100

    project created

---
+++ src/main/java/com/lbg/PersonalProject/HackathonBeApplication.java
@@ -0,0 +1,13 @@
+package com.lbg.PersonalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonBeApplication.class, args);
+	}
+
+}
