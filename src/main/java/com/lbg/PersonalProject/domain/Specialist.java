0a5397830ac75553f8514842d2d17dbea13d3612
unknown <mikeatkoo01@gmail.com>
2024-05-08 15:39:57 +0100

    added tampa

--- src/main/java/com/lbg/PersonalProject/domain/Specialist.java
+++ src/main/java/com/lbg/PersonalProject/domain/Specialist.java
@@ -15,7 +15,7 @@ public class Specialist {
 	private Integer id;
 	private String name;
 	private String specialism;
-	private String specialist;
+	private String specialists;
 	private String date;
 	private String time;
 
@@ -23,86 +23,50 @@ public class Specialist {
 		super();
 	}
 
-	/**
-	 * @return the id
-	 */
 	public Integer getId() {
 		return id;
 	}
 
-	/**
-	 * @param id the id to set
-	 */
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
-	/**
-	 * @return the name
-	 */
 	public String getName() {
 		return name;
 	}
 
-	/**
-	 * @param name the name to set
-	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	/**
-	 * @return the specialism
-	 */
 	public String getSpecialism() {
 		return specialism;
 	}
 
-	/**
-	 * @param specialism the specialism to set
-	 */
 	public void setSpecialism(String specialism) {
 		this.specialism = specialism;
 	}
 
-	/**
-	 * @return the specialist
-	 */
-	public String getSpecialist() {
-		return specialist;
+	public String getSpecialists() {
+		return specialists;
 	}
 
-	/**
-	 * @param specialist the specialist to set
-	 */
-	public void setSpecialist(String specialist) {
-		this.specialist = specialist;
+	public void setSpecialists(String specialists) {
+		this.specialists = specialists;
 	}
 
-	/**
-	 * @return the date
-	 */
 	public String getDate() {
 		return date;
 	}
 
-	/**
-	 * @param date the date to set
-	 */
 	public void setDate(String date) {
 		this.date = date;
 	}
 
-	/**
-	 * @return the time
-	 */
 	public String getTime() {
 		return time;
 	}
 
-	/**
-	 * @param time the time to set
-	 */
 	public void setTime(String time) {
 		this.time = time;
 	}
